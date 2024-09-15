a356aad4a8ee3990d3b0d45af3f364a51c6f829d
Topias Heinonen <topias.heinonen@gofore.com>
2024-09-15 23:37:56 +0300

    Blink every 250ms with 10ms interrupts

--- blink.s
+++ blink.s
@@ -1,3 +1,4 @@
+; I/O addresses
 PORTB = $6000 ; Data for port B
 PORTA = $6001 ; Data for port A
 DDRB  = $6002 ; Data direction register for port B
@@ -6,6 +7,11 @@ T1CL  = $6004 ; Timer 1 counter low
 T1CH  = $6005 ; Timer 1 counter high
 ACR   = $600B ; Auxiliary control register
 IFR   = $600D ; Interrupt flag register
+IER   = $600E ; Interrupt enable register
+
+; RAM addresses
+ticks = $00   ; timer ticks, 4 bytes up to $03
+toggle_time = $04  ; 1 byte
 
   .org $8000
 
@@ -14,26 +20,50 @@ reset:
   sta DDRA
   lda #0
   sta PORTA
-  sta ACR
-  
+  jsr init_timer
+
 loop:
-  inc PORTA
-  jsr delay
-  dec PORTA
-  jsr delay
+  sec
+  lda ticks
+  sbc toggle_time
+  cmp #25
+  bcc loop
+
+  lda #$01
+  eor PORTA
+  STA PORTA
+  lda ticks
+  sta toggle_time
   jmp loop
 
-delay:
-  lda #$50
+init_timer:
+  stz ticks
+  stz ticks + 1
+  stz ticks + 2
+  stz ticks + 3
+  lda #%01000000
+  sta ACR
+  lda #$0e
   sta T1CL
-  lda #$c3
+  lda #$27
   sta T1CH
-delay1:
-  bit IFR
-  bvc delay1
-  lda T1CL
+  lda %11000000
+  sta IER
+  cli
   rts
+
+irq:
+  bit T1CL
+  inc ticks
+  bne end_irq
+  inc ticks + 1
+  bne end_irq
+  inc ticks + 2
+  bne end_irq
+  inc ticks + 3
+end_irq:
+  rti
 
   .org $fffc
   .word reset
-  .word $0000
+  .word irq
