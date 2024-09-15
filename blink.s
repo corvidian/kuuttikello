99a648f0019575314766034435e5ac8b87307a0a
Topias Heinonen <topias.heinonen@gofore.com>
2024-09-15 22:53:38 +0300

    Blink fixed delay by loop

--- blink.s
+++ blink.s
@@ -1,18 +1,38 @@
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+E  = %10000000
+RW = %01000000
+;RS = %00100000
+
   .org $8000
+
 reset:
   lda #$ff
-  sta $6002
+  sta DDRA
+  lda #0
+  sta PORTA
   
-  sta $6000
-
-  tax
-
 loop:
-
-  inx
-  stx $6000
-
+  inc PORTA
+  jsr delay
+  dec PORTA
+  jsr delay
   jmp loop
+
+delay:
+  ldy #$ff
+delay2:
+  ldx #$ff
+delay1:
+  nop
+  dex
+  bne delay1
+  dey
+  bne delay2
+  rts
 
   .org $fffc
   .word reset
